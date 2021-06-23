1cb2cc6017ff440ab4e499272eff45de4e29e8fa
Pascal Gerig <pgerig@fastforward.ch>
2021-06-23 17:05:07 +0200

    Add distributed hello world and primitive Dockerization

--- price-service/src/main/java/price/service/PriceController.java
+++ price-service/src/main/java/price/service/PriceController.java
@@ -3,12 +3,31 @@ package price.service;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+
+import javax.inject.Inject;
 
 @Controller("/")
 public class PriceController {
 
+    @Client("http://localhost:8080")
+    @Inject
+    RxHttpClient orderHttpClient;
+
+    @Client("http://localhost:8082")
+    @Inject
+    RxHttpClient productHttpClient;
+
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
         return "Hello World";
+    }
+
+    @Get(value = "/distributed", produces = MediaType.TEXT_PLAIN)
+    public String distributedIndex() {
+        String first = orderHttpClient.toBlocking().retrieve("/");
+        String second = productHttpClient.toBlocking().retrieve("/");
+        return first + second;
     }
 }
--- product-service/src/main/java/product/service/ProductController.java
+++ product-service/src/main/java/product/service/ProductController.java
@@ -3,12 +3,31 @@ package product.service;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+
+import javax.inject.Inject;
 
 @Controller("/")
 public class ProductController {
 
+    @Client("http://localhost:8080")
+    @Inject
+    RxHttpClient orderHttpClient;
+
+    @Client("http://localhost:8081")
+    @Inject
+    RxHttpClient priceHttpClient;
+
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
         return "Hello World";
+    }
+
+    @Get(value = "/distributed", produces = MediaType.TEXT_PLAIN)
+    public String distributedIndex() {
+        String first = orderHttpClient.toBlocking().retrieve("/");
+        String second = priceHttpClient.toBlocking().retrieve("/");
+        return first + second;
     }
 }
